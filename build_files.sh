dfd8580aeb084cc31bcbe5274e4deb69ae05921a
Hakunamatata40 <yanavadieudonne@gmail.com>
2025-08-27 17:39:54 +0100

    mon porfolio integration de vercel 10

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "BUILD START"
-python3.11 -m pip install --upgrade pip
-python3.11 -m pip install -r requirements.txt
+# Upgrade pip
+python3 -m pip install --upgrade pip
+# Install dependencies
+python3 -m pip install -r requirements.txt
 echo "Running collectstatic..."
-python3.11 manage.py collectstatic --noinput --clear --verbosity 3
+python3 manage.py collectstatic --noinput --clear --verbosity 3
 if [ $? -eq 0 ]; then
     echo "collectstatic completed successfully"
 else
