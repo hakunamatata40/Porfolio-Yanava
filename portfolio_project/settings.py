effb213be7118439422a3ba28fc539383d37f7da
Hakunamatata40 <yanavadieudonne@gmail.com>
2025-08-27 17:48:54 +0100

    mon porfolio integration de vercel 11

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&yfmdv9&a0m*=^p#(4xfd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1', 'porfolio-yanava-ibzloxe9y-yanava-dieudonne-s-projects.vercel.app']
 
 
 # Application definition
