6168c201fb158dca5e781fc9281a04809b77bef4
Hakunamatata40 <yanavadieudonne@gmail.com>
2025-08-27 04:17:59 +0100

    mon porfolio integration de vercel 2

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -139,3 +139,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'yanavadieudonne@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'yanavadieudonne@gmail.com')
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
